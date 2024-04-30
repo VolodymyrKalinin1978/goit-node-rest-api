fe6ea395726390805f4bc955f740e44486f9b114
volodymyrkalinin1978 <122758451+VolodymyrKalinin1978@users.noreply.github.com>
2024-05-01 02:01:13 +0300

    Update Ver. 1.6

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -34,7 +34,7 @@ export const verifyEmail = async (req, res, next) => {
   }
   await User.findByIdAndUpdate(user._id, {
     verify: true,
-    verificationToken: "",
+    verificationToken: null,
   });
 
   res.status(200).json({
